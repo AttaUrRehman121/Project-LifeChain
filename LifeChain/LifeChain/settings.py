0103a7a8e2143463202a217fb768fa8092d6ec53
ATTA UR REHMAN <manirana770@gmail.com>
2025-02-09 11:15:00 +0500

    static files gets throuh applying python manage.py collectstatic command

--- LifeChain/LifeChain/settings.py
+++ LifeChain/LifeChain/settings.py
@@ -133,13 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
-
-
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
 MEDIA_ROOT = BASE_DIR / "media"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
